91dc5119bb71f73e987d37657f7f3211bdc612d9
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2018-04-29 15:01:15 +0700

    Pretify

--- src/Models/Bantenprov/Zona/MasterZona.php
+++ src/Models/Bantenprov/Zona/MasterZona.php
@@ -28,6 +28,6 @@ class MasterZona extends Model
 
     public function user()
     {
-        return $this->belongsTo('App\User','user_id');
+        return $this->belongsTo('App\User', 'user_id');
     }
 }
--- src/Models/Bantenprov/Zona/Zona.php
+++ src/Models/Bantenprov/Zona/Zona.php
@@ -42,17 +42,17 @@ class Zona extends Model
 
     public function siswa()
     {
-        return $this->belongsTo('Bantenprov\Siswa\Models\Bantenprov\Siswa\Siswa','nomor_un','nomor_un');
+        return $this->belongsTo('Bantenprov\Siswa\Models\Bantenprov\Siswa\Siswa', 'nomor_un', 'nomor_un');
     }
 
     public function sekolah()
     {
-        return $this->belongsTo('Bantenprov\Sekolah\Models\Bantenprov\Sekolah\Sekolah','sekolah_id');
+        return $this->belongsTo('Bantenprov\Sekolah\Models\Bantenprov\Sekolah\Sekolah', 'sekolah_id');
     }
 
     public function user()
     {
-        return $this->belongsTo('App\User','user_id');
+        return $this->belongsTo('App\User', 'user_id');
     }
 
     public function nilai($lokasi_siswa, $lokasi_sekolah)
